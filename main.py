d9f0d49af7bec2ad1ba62d42a78b69f8a63a6390
Nikolett Jávor <71188513+njavor@users.noreply.github.com>
2022-01-28 12:53:01 +0100

    -main be másolt Gábor.py

--- main.py
+++ main.py
@@ -21,37 +21,126 @@ with open("input.txt","r",encoding="utf8") as f:
 		s = sor.strip().split(";")
 		t = Tanulo(int(s[0]), s[1], s[2], s[3], s[4], s[5], int(s[6]), int(s[7]))
 
-"""
-print("1) Hány diák tanul az osztályban?")
-print("2) Hány fiú tanul az osztályban?")
-print("3) Hány lány tanul az osztályban?")
-print("4) Hány olyan diák van, akiknek több mint 1 testvére van!")
-print("5) Gyűjtse ki azon diákok nevét, akiknek több mint 1 testvérük van!")
-print("6) Hány olyan diák van, akiknek több mint 2 testvére van!")
-print("7) Gyűjtse ki azon diákok nevét, akiknek több mint 2 testvérük van!")
-print("8) Hány olyan diák van, akik a 2. idegen nyelvként a németet tanulják!")
-print("9) Gyűjtse ki azon fiú diákok nevét, akik a 2. idegen nyelvként a németet tanulják!")
+#10.
+egyes_a_számolás=0
+for elem in Tanulo.lista:
+    if elem.angolcs=="1. Sió":
+        egyes_a_számolás+=1
+
 print("10) Hány diák tanul, az egyes angol csoportban?")
+print(egyes_a_számolás)
+#-----------------------------------
+
+#11.
+kettes_a_számolás=0
+for elem in Tanulo.lista:
+    if elem.angolcs=="2. Bán":
+        kettes_a_számolás+=1
+
 print("11) Hány diák tanul, a kettes angol csoportban?")
+print(kettes_a_számolás)
+#----------------------------------------
+
+#12
+
+alfa_matek_számolás=0
+for elem in Tanulo.lista:
+    if elem.mVt=="alfa":
+        alfa_matek_számolás+=1
+
 print("12) Hány diák tanul, az alfa matematika csoportban?")
+print(alfa_matek_számolás)
+#---------------------------------------
+
+#13
+
+beta_matek_számolás=0
+for elem in Tanulo.lista:
+    if elem.mVt=="beta":
+        beta_matek_számolás+=1
+
 print("13) Hány diák tanul, az beta matematika csoportban?")
+print(beta_matek_számolás)
+#----------------------------------------
+
+#14
+
+alfa_matek_lányok =0
+for elem in Tanulo.lista:
+    if elem.nem=="L" and elem.mVt=="alfa":
+        alfa_matek_lányok+=1
+
 print("14) Hány lány diák tanul, az alfa matematika csoportban?")
+print(alfa_matek_lányok)
+#-----------------------------------------
+
+#15
+
+beta_matek_lányok =0
+for elem in Tanulo.lista:
+    if elem.nem=="L" and elem.mVt=="beta":
+        beta_matek_lányok+=1
+
 print("15) Hány lány diák tanul, a beta matematika csoportban?")
+print(beta_matek_lányok)
+#--------------------------------
+
+#16
+alfa_matek_fiuk =0
+for elem in Tanulo.lista:
+    if elem.nem=="F" and elem.mVt=="alfa":
+        alfa_matek_fiuk+=1
+
 print("16) Hány fiú diák tanul, az alfa matematika csoportban?")
+print(alfa_matek_fiuk)
+#--------------------------------
+
+#17
+
+beta_matek_fiuk =0
+for elem in Tanulo.lista:
+    if elem.nem=="F" and elem.mVt=="beta":
+        beta_matek_fiuk+=1
 print("17) Hány fiú diák tanul, a beta matematika csoportban?")
+print(beta_matek_fiuk)
+#------------------------------------
+
+#18
+
+oroszosok=False
+i=0
+while(not oroszosok and i<len(Tanulo.lista)):
+    if Tanulo.lista[i]=="orosz":
+        oroszosok=True
+    i+=1
+
 print("18) Van-e olyan diák, aki a 2. idegen nyelvként oroszt tanul?")
+print(oroszosok)
+#------------------------------------
+
+#19
+
+olaszosok=False
+j=0
+while(not olaszosok and j<len(Tanulo.lista)):
+    if Tanulo.lista[j]=="olasz":
+        oroszosok=True
+    j+=1
+
 print("19) Van-e olyan diák, aki a 2. idegen nyelvként olaszt tanul?")
+print (olaszosok)
+#-------------------------------------------
+
+#20
+
+
+spanyolosok=False
+h=0
+while(not spanyolosok and j<len(Tanulo.lista)):
+    if Tanulo.lista[h]=="spanyol":
+        spanyolosok=True
+    h+=1
+
 print("20) Van-e olyan diák, aki a 2. idegen nyelvként spanyolt tanul?")
-print("21) Mekkora a legnagyobb család az osztályban?")
-print("22) Írjuk ki az egyik olyan diák nevét akinek e legtöbb testvére van!")
-print("23) Gyűjtse ki azon lány diákok nevét, akik az egyes vagy kettes angol csoportban vannak!")
-print("24) Gyűjtse ki azon fiú diákok nevét, akik a hármas vagy négyes angol csoportban vannak és 0 vagy 2 testvérük van!")
-print("25) Viszonylag kevés azon családok száma, ahol az együttlakók száma és a testvérek száma között nem három a különbség. Adja meg a számukat!")
-print("26) Dári Dóra hiányzott a legutóbbi angol órán, szeretné bepótolni a hiányzást. Adja meg azon tanulók nevét, akik vele azonos angol csoportba járnak.")
-print("27) Avon Mór hiányzott a legutóbbi angol órán, szeretné bepótolni a hiányzást. Adja meg azon tanulók nevét, akik vele azonos angol csoportba járnak. ")
-print("28) Zúz Mara hiányzott a legutóbbi angol órán, szeretné bepótolni a hiányzást. Adja meg azon tanulók nevét, akik vele azonos angol csoportba járnak. ")
-print("29) Citad Ella hiányzott a legutóbbi angol órán, szeretné bepótolni a hiányzást. Adja meg azon tanulók nevét, akik vele azonos angol csoportba járnak.")
-print("30) Hát Izsák hiányzott a legutóbbi angol órán, szeretné bepótolni a hiányzást. Adja meg azon tanulók nevét, akik vele azonos angol csoportba járnak.")
-print("31) A spanyol vagy a német nyelvet tanulják-e többben az osztáyban?")
-print("32) Kérjen be a felhasználótól egy nyelvet és írja ki, az adott nyelvet tanulók névsorát!")
-"""
+print(spanyolosok)
+#----------------------------------
